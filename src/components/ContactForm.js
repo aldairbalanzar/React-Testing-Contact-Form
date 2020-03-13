ab432a1510c683cfbf21e1440ee5f8f7493d56be
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-03-12 23:17:03 -0400

    added more tests to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -19,7 +19,7 @@ const ContactForm = () => {
             id="firstName"
             name="firstName"
             placeholder="bill"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, maxLength: 20 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -67,7 +67,7 @@ const ContactForm = () => {
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submit" type="submit" />
       </form>
     </div>
   );
